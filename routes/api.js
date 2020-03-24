6e89ec1255691279fbec1823bfb250310c62e4ef
Crystal Danger <cdneal091@gmail.com>
2020-03-24 15:34:47 -0400

    add api routes

---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router()
+
+router.get('/api', function (req, res) {
+
+})
+
+router.get('/api/blog', function (req, res) {
+
+})
+
+router.post('/api/blog', function (req, res) {
+
+})
+
+router.put('api/blog/:id', function (req, res) {
+
+})
+
+router.delete('api/blog/:id', function (req, res) {
+
+})
+
+module.exports = router
