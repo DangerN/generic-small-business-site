8f8e6dd7b1d3812a80b6ac6b6e44b3ae0583686d
Crystal Danger <cdneal091@gmail.com>
2020-03-24 16:41:22 -0400

    connected api

--- routes/api.js
+++ routes/api.js
@@ -4,23 +4,23 @@ const router = express.Router()
 router.use(express.json())
 router.use(express.urlencoded({extended: true}))
 
-router.get('/api', function (req, res) {
+router.get('/', function (req, res) {
+  res.send('api ok!')
+})
+
+router.get('/blog', function (req, res) {
 
 })
 
-router.get('/api/blog', function (req, res) {
+router.post('/blog', function (req, res) {
 
 })
 
-router.post('/api/blog', function (req, res) {
+router.put('/blog/:id', function (req, res) {
 
 })
 
-router.put('api/blog/:id', function (req, res) {
-
-})
-
-router.delete('api/blog/:id', function (req, res) {
+router.delete('/blog/:id', function (req, res) {
 
 })
 
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const port = process.env.port || 3000
 const api = require('./routes/api')
 
 app.use('/', express.static('public/main/build'))
+app.use('/api', api)
 
 app.listen(port, () => {
   console.log(`Ready to go on port ${port}!`);
