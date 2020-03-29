5d8724dc2acf7dba4d86907493227fbe70b15138
Crystal Danger <cdneal091@gmail.com>
2020-03-28 23:38:39 -0400

    need to figure out how to set up and push to custom branch with only build and stop tracking it. yikes

---
+++ public/main/build/precache-manifest.5b4b3f27e74fd152ed6a415c7cb6ab6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d101ce5eb897aa2898c3efdd52af1172",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6319166c898732b4504f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5236615f29fd5cec9dc2",
+    "url": "/static/js/2.f5cda060.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f5cda060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6319166c898732b4504f",
+    "url": "/static/js/main.1c49eadc.chunk.js"
+  },
+  {
+    "revision": "13763119a5f7e522a0e0",
+    "url": "/static/js/runtime-main.bae28164.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
