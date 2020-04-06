e5f3b46c0f28a81870104ce27eefeb85bd315868
Crystal Danger <cdneal091@gmail.com>
2020-04-06 16:45:06 -0400

    ensure react tests are running correctly

--- public/main/src/App.test.js
+++ public/main/src/App.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { shallow } from 'enzyme'
 import App from './App';
 
-test('passes', () => {
-  expect(true).toEqual(true)
+it('renders', () => {
+  shallow(<App />)
 });
--- test/orm.test.js
+++ test/orm.test.js
@@ -42,5 +42,3 @@ describe('product', function () {
     })
   })
 })
-
-// const
