0fa9606098e7c297eccfc9b7ca02532db12ccbee
Crystal Danger <cdneal091@gmail.com>
2020-04-14 13:58:10 -0400

    link to cart page

--- public/main/src/App.js
+++ public/main/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navi from './components/Navi'
 import Landing from './components/Landing'
+import Cart from './components/Cart'
 import NotFoundPage from './components/NotFoundPage'
 import './App.css';
 import { useRoutes } from 'hookrouter'
 
 const routes = {
   '/': () => <Landing />,
+  '/cart': () => <Cart />
 }
 
 function App() {
