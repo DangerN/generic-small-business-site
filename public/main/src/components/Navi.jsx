bc6f1bb882bf2a11c2a650432deaf6be85869333
Crystal Danger <cdneal091@gmail.com>
2020-04-14 13:56:25 -0400

    flesh out nav bar

--- public/main/src/components/Navi.jsx
+++ public/main/src/components/Navi.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { A } from 'hookrouter'
+import { FaShoppingCart, FaUserCircle } from 'react-icons/fa'
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
 
 const Navi = props => {
@@ -13,9 +14,13 @@ const Navi = props => {
           <Nav.Link href='/contact' as={A} >Contact</Nav.Link>
         </Nav>
         <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="outline-success">Search</Button>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="outline-success">Search</Button>
         </Form>
+        <Nav>
+          <Nav.Link href='/cart' as={A} ><FaShoppingCart/></Nav.Link>
+          <Nav.Link href='/user' as={A} ><FaUserCircle/><div>Sign In</div></Nav.Link>
+        </Nav>
       </Navbar.Collapse>
     </Navbar>
   )
