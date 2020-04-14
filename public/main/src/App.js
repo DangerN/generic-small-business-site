2d9197963525269db50aeccec2f4ebce86a176f9
Crystal Danger <cdneal091@gmail.com>
2020-04-14 14:49:36 -0400

    add about and contact pages

--- public/main/src/App.js
+++ public/main/src/App.js
@@ -3,12 +3,16 @@ import { useRoutes } from 'hookrouter'
 import Navi from './components/Navi'
 import Landing from './components/Landing'
 import Cart from './components/Cart'
+import Contact from './components/Contact'
+import About from './components/About'
 import NotFoundPage from './components/NotFoundPage'
 import './App.css';
 
 const routes = {
   '/': () => <Landing />,
-  '/cart': () => <Cart />
+  '/cart': () => <Cart />,
+  '/contact': () => <Contact />,
+  '/about': () => <About />
 }
 
 function App() {
