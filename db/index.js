37b801c29f3e75cae4794ad69b93c62b94f27202
Crystal Danger <cdneal091@gmail.com>
2020-04-15 13:46:43 -0400

    loggin connection string

--- db/index.js
+++ db/index.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
 const connectionString = process.env.DATABASE_URL
 
+console.log(process.env.DATABASE_URL);
+
 const pool = new Pool({connectionString: connectionString})
 
 module.exports = {
