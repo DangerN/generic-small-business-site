b1cf6fd689b9055df3a50d92008cba57a7db72fc
Crystal Danger <cdneal091@gmail.com>
2020-04-26 12:43:55 -0400

    converted from hookrouter to react-router-dom

--- public/main/src/components/Navi.jsx
+++ public/main/src/components/Navi.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
-import { A } from 'hookrouter'
+import { Link } from 'react-router-dom'
 import { FaShoppingCart, FaUserCircle } from 'react-icons/fa'
 import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
 
 const Navi = props => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href='/' as={A}>GBusiness</Navbar.Brand>
+      <Navbar.Brand to='/' as={Link}>GBusiness</Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse id='basic-navbar-nav'>
         <Nav className='mr-auto'>
-          <Nav.Link href='/about' as={A} >About</Nav.Link>
-          <Nav.Link href='/contact' as={A} >Contact</Nav.Link>
+          <Nav.Link to='/about' as={Link} >About</Nav.Link>
+          <Nav.Link to='/contact' as={Link} >Contact</Nav.Link>
         </Nav>
         <Form inline>
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form>
         <Nav>
-          <Nav.Link as={A} href='/cart' ><FaShoppingCart size='38'/></Nav.Link>
-          <Nav.Link href='/user' as={A} ><FaUserCircle/><div>Sign In</div></Nav.Link>
+          <Nav.Link as={Link} to='/cart' ><FaShoppingCart size='38'/></Nav.Link>
+          <Nav.Link to='/user' as={Link} ><FaUserCircle/><div>Sign In</div></Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
