387903558cc3a741931de01880925de2f4592889
Crystal Danger <cdneal091@gmail.com>
2020-05-06 13:29:40 -0400

    adding functionally to dashboard

--- public/dashboard/src/App.js
+++ public/dashboard/src/App.js
@@ -1,11 +1,35 @@
-import React from 'react';
-import './App.css';
+import React, {useEffect} from 'react';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
+import Container from 'react-bootstrap/Container'
+import StoreTab from './components/tabs/StoreTab'
+import axios from 'axios'
+import useStore from './hooks/useStore'
+
+const BASE_PATH = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:4000'
 
 function App() {
+  const [ storeState, storeDispatch ] = useStore()
+
+  useEffect(() => {
+    axios(`${BASE_PATH}/api/store/products`)
+    .then(({data})=>storeDispatch({type: 'setProducts', payload: data}))
+  },[storeDispatch])
+
   return (
-    <div className="App">
-      This should be the dashboard.
-    </div>
+    <Container>
+      <Tabs defaultActiveKey="store">
+        <Tab eventKey="store" title="Store">
+          <StoreTab {...storeState}/>
+        </Tab>
+        <Tab eventKey="messages" title="Messages">
+          Yeet
+        </Tab>
+        <Tab eventKey="orders" title="Orders">
+          Orders
+        </Tab>
+      </Tabs>
+    </Container>
   );
 }
 
--- public/dashboard/src/components/tabs/StoreTab.jsx
+++ public/dashboard/src/components/tabs/StoreTab.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import Table from 'react-bootstrap/Table'
 
 const StoreTab = props => {
+  console.log(props.products);
+  const makeProductTable = () => {
+    return props.products.map(({id, name, catagory, price, ship_cost, stock})=>{
+      return (
+        <tr onClick={()=>console.log(id)}>
+          <td>{id}</td>
+          <td>{name}</td>
+          <td>{catagory}</td>
+          <td>{price}</td>
+          <td>{ship_cost}</td>
+          <td>{stock}</td>
+        </tr>
+      )
+    })
+  }
+
   return (
-    <div>
-      StoreTab
-    </div>
+    <Row>
+      <Col xs={2}>
+        <Button>Add Product</Button>
+
+      </Col>
+      <Col xs={10}>
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Catagory</th>
+              <th>Price</th>
+              <th>Ship Cost</th>
+              <th>Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {makeProductTable()}
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
   )
 }
 
