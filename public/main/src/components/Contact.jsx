d6e1caf011e60c6281511fe7d4b1819d0a66325d
Crystal Danger <cdneal091@gmail.com>
2020-05-10 13:31:57 -0400

    main site can now send messages

--- public/main/src/components/Contact.jsx
+++ public/main/src/components/Contact.jsx
@@ -3,35 +3,39 @@ import Form from 'react-bootstrap/Form'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
+import Alert from 'react-bootstrap/Alert'
 import useControlledContact from '../hooks/useControlledContact'
 import useColor from '../hooks/useColor'
 
 
 const Contact = props => {
-  const { name, subject, email, message, submit } = useControlledContact()
+  const { name, subject, email, message, submit, alert } = useControlledContact()
   return (
-    <Jumbotron className='bg-light' style={{borderTopRadius: '0px'}}>
-      <p>
-        Please feel free to contact us using the form below.
-      </p>
-      <Form {...submit}>
-        <Form.Row>
-          <Form.Group as={Col}>
-            <Form.Control {...name} placeholder='Name' />
+    <>
+      <Jumbotron className='bg-light' style={{borderTopRadius: '0px'}}>
+        <p>
+          Please feel free to contact us using the form below.
+        </p>
+        <Form {...submit}>
+          <Form.Row>
+            <Form.Group as={Col}>
+              <Form.Control {...name} placeholder='Name' />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Control {...email} placeholder='Email' type='email' />
+            </Form.Group>
+          </Form.Row>
+          <Form.Group>
+            <Form.Control {...subject} placeholder='Subject'/>
           </Form.Group>
-          <Form.Group as={Col}>
-            <Form.Control {...email} placeholder='Email' type='email' />
+          <Form.Group>
+            <Form.Control {...message} style={{minHeight: '10rem'}} as='textarea' placeholder="Message"/>
           </Form.Group>
-        </Form.Row>
-        <Form.Group>
-          <Form.Control {...subject} placeholder='Subject'/>
-        </Form.Group>
-        <Form.Group>
-          <Form.Control {...message} style={{minHeight: '10rem'}} as='textarea' placeholder="Message"/>
-        </Form.Group>
-        <Button {...useColor('button')} type='submit'>Send!</Button>
-      </Form>
-    </Jumbotron>
+          <Button {...useColor('button')} type='submit'>Send!</Button>
+        </Form>
+      </Jumbotron>
+      <Alert {...alert.props} >{alert.message}</Alert>
+    </>
   )
 }
 
--- public/main/src/hooks/useControlledContact.jsx
+++ public/main/src/hooks/useControlledContact.jsx
@@ -1,10 +1,40 @@
 import { useState } from 'react'
+import axios from 'axios'
+import { BASE_PATH } from '../constants'
 
 export default () => {
   const [ name, setName ] = useState('')
   const [ email, setEmail ] = useState('')
   const [ subject, setSubject ] = useState('')
   const [ message, setMessage ] = useState('')
+
+  const initAlert = {props: {show:false}}
+  const [ alert, setAlert ] = useState(initAlert)
+
+  const resetForm = () => {
+    setName('')
+    setEmail('')
+    setSubject('')
+    setMessage('')
+  }
+
+  const alertDeets = {
+    success: {
+      message: 'Your message has been recieved, someone will contact you shortly.',
+      props:  {
+        show: true,
+        variant: 'success'
+      }
+    },
+    failure: {
+      message: 'Something went wrong. Please try again later',
+      props:  {
+        show: true,
+        variant: 'danger'
+      }
+    }
+  }
+
   return {
     name: {
       value: name,
@@ -25,8 +55,25 @@ export default () => {
     submit: {
       onSubmit: event=>{
         event.preventDefault()
-        console.log(event.target);
-      },
-    }
+        axios({
+          method: 'post',
+          url: `${BASE_PATH}/api/messages`,
+          data: {
+            name: name,
+            email: email,
+            subject: subject,
+            body: message
+          }
+        }).then(()=>{
+          setAlert(alertDeets.success)
+          setTimeout(()=>setAlert(initAlert), 3000)
+          resetForm()
+        }).catch(()=>{
+          setAlert(alertDeets.failure)
+          setTimeout(()=>setAlert(initAlert), 3000)
+        })
+      }
+    },
+    alert: alert
   }
 }
